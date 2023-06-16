0f7d85c90434d686abd727856737da1b96f3b255
Gabriel Ferreira <56842000+GabrielFerreira7@users.noreply.github.com>
2023-06-16 10:06:17 -0300

    first commit

---
+++ server1.py
@@ -0,0 +1,70 @@
+from typing import Dict, Optional, Tuple
+import flwr as fl
+import utils
+
+SERVER_ADDRESS = "[::]:8080" # dessa forma é possível acessar o servidor pelo Ip externo e interno
+
+def main() -> None:
+    model = utils.get_model()
+    model.compile("adam", "binary_crossentropy", metrics=["accuracy"])
+
+    strategy = fl.server.strategy.FedAvg(
+        fraction_fit=0.2,
+        fraction_evaluate=0.2,
+        min_fit_clients=2,
+        min_evaluate_clients=2,
+        min_available_clients=2,
+        evaluate_fn=get_evaluate_fn(model),
+        on_fit_config_fn=fit_config,
+        on_evaluate_config_fn=evaluate_config,
+        initial_parameters=fl.common.ndarrays_to_parameters(model.get_weights()),
+    )
+
+    fl.server.start_server(
+        server_address="[::]:8080",
+        config=fl.server.ServerConfig(num_rounds=4),
+        strategy=strategy,
+    )
+
+
+def get_evaluate_fn(model):
+    """Return an evaluation function for server-side evaluation."""
+    _, (x_test, y_test) = utils.load_data()
+    # The `evaluate` function will be called after every round
+    def evaluate(
+        server_round: int,
+        parameters: fl.common.NDArrays,
+        config: Dict[str, fl.common.Scalar],
+    ) -> Optional[Tuple[float, Dict[str, fl.common.Scalar]]]:
+        model.set_weights(parameters)  # Update model with the latest parameters
+        loss, accuracy = model.evaluate(x_test, y_test)
+        return loss, {"test_accuracy": accuracy}
+
+
+    return evaluate
+
+
+def fit_config(rnd: int):
+    """Return training configuration dict for each round.
+
+    Keep batch size fixed at 32, perform two rounds of training with one
+    local epoch, increase to two local epochs afterwards.
+    """
+    config = {
+        "batch_size": 32,
+        "local_epochs": 1 if rnd < 2 else 4,
+    }
+    return config
+
+
+def evaluate_config(rnd: int):
+    """Return evaluation configuration dict for each round.
+
+    Perform five local evaluation steps on each client (i.e., use five
+    batches) during rounds one to three, then increase to ten local
+    evaluation steps.
+    """
+    val_steps = 5 if rnd < 4 else 10
+    return {"val_steps": val_steps}
+
+main()
